9a77aa4e07cedb58d5edb38f8cbe3e14f187842d
NathanM2035 <74957883+NathanM2035@users.noreply.github.com>
2020-11-24 11:38:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 "use strict";
 
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./pwa-examples/js13kpwa/sw.js');
-};
-
 /************************************************************************
  *                                                                      *
  *                  Ginko Bus Progressive Web Application               *
